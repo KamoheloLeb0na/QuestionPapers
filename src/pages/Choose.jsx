085d3b8ccd70e569397e26b874cc19370f791232
Kamohelo Lebona <kamohelolebna@gmail.com>
2023-07-18 00:17:36 +0200

    Minor change

--- src/pages/Choose.jsx
+++ src/pages/Choose.jsx
@@ -5,8 +5,8 @@ export default function Choose(){
     return(
         <div>
             <Grid container spacing={2}>
-                <Grid item xs={6} sm={6} md={3}><div style={{display:"flex",justifyContent:"center",alignItems:"center"}}> <Link to="/lgcse2" style={{border:"solid",borderRadius:"10px",width:"100%"}}><Button sx={{width:"100%"}}>  LGCSE 2  </Button></Link> </div></Grid>
-                <Grid item xs={6} sm={6} md={3}><div style={{display:"flex",justifyContent:"center",alignItems:"center"}}> <Link to="/lgcse4" style={{border:"solid",borderRadius:"10px",width:"100%"}}><Button sx={{width:"100%"}}>LGCSE 4</Button></Link> </div></Grid>
+                <Grid item xs={6} sm={6} md={3}><div style={{display:"flex",justifyContent:"center",alignItems:"center"}}> <Link to="/lgcse2" style={{border:"solid",borderRadius:"10px",width:"100%"}}><Button sx={{width:"100%"}}>  LGCSE Paper2  </Button></Link> </div></Grid>
+                <Grid item xs={6} sm={6} md={3}><div style={{display:"flex",justifyContent:"center",alignItems:"center"}}> <Link to="/lgcse4" style={{border:"solid",borderRadius:"10px",width:"100%",textDecoration:"none",color:"blue"}}><Button sx={{width:"100%"}}>LGCSE Paper4</Button></Link> </div></Grid>
                 <br />
                 <Grid item xs={12} sm={6} md={3}><div style={{display:"flex",justifyContent:"center",alignItems:"center"}}><Button sx={{border:"solid",borderRadius:"10px",width:"100%"}}>Notes</Button></div></Grid>
                 <br />
